d6edded543eafb5b4a34cd854d4c71e43c8425a3
wiktor <nowak.wiktor95@gmail.com>
2026-07-19 13:45:34 +0200

    Initial.

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
+        <h1 className="text-3xl font-semibold text-black dark:text-zinc-50">
+          labb_simple
+        </h1>
+      </main>
+    </div>
+  );
+}
